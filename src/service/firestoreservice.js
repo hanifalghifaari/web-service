badb0f37b9f894305b1b8e93e7045c94e782bcf9
hanifalghifaari <168663443+hanifalghifaari@users.noreply.github.com>
2024-12-01 15:53:07 +0700

    Delete src directory

--- src/service/firestoreservice.js
+++
@@ -1,9 +0,0 @@
-const { Firestore } = require('@google-cloud/firestore');
-
-async function storeData(id, data) {
-  const db = new Firestore();
-  const predictionsCollection = db.collection('predictions');
-  return predictionsCollection.doc(id).set(data);
-}
-
-module.exports = storeData;
